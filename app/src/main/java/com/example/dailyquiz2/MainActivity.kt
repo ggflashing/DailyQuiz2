8c1b7da8c987a26bd8669d7890fc12a6f6dc3298
ggflashing <salih88825fg@gmail.com>
2026-01-07 11:11:36 +0600

    Update 07.01.2026 added @RequiresApi(Build.VERSION_CODES.O)

--- app/src/main/java/com/example/dailyquiz2/MainActivity.kt
+++ app/src/main/java/com/example/dailyquiz2/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.dailyquiz2
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +25,9 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
