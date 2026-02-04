a110afeee25d0a949e93f835fe50e356f1614287
ggflashing <salih88825fg@gmail.com>
2026-02-04 14:35:50 +0600

    Update 04.02.2026 result_screen

---
+++ app/src/main/java/com/example/dailyquiz2/presentation/History_Quiz/navigate_history_screen.kt
@@ -0,0 +1,9 @@
+package com.example.dailyquiz2.presentation.History_Quiz
+
+
+
+data class navigate_history_screen(
+
+    val screenState_history_models_result: Quiz_history_navigation_screen = Quiz_history_navigation_screen.history
+
+)
